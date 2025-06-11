a14713d4f1a1e543315fe911d684936ca12fb5a2
Elena Zapodoynikova <lenazew283@gmail.com>
2025-06-12 10:23:09 +1100

    Update conf.py

--- pokemonbatlle-main/common/conf.py
+++ pokemonbatlle-main/common/conf.py
@@ -5,12 +5,12 @@ class Cfg:
     URL ='https://pokemonbattle-stage.ru'
     API_URL = 'https://api.pokemonbattle-stage.ru/v2'
     VALID = {
-        'email': 'zew2831@yandex.ru',         # type your valid email
-        'password': 'Zap83wer%'       # type your valid password
+        'email': 'your email',         # type your valid email
+        'password': 'your password'       # type your valid password
     }
     INVALID = {
-        'email': 'zew2831yandex.ru',          # type invalid email
-        'password': 'Zap83werd'     # type invalid password
+        'email': 'your invalid email',          # type invalid email
+        'password': 'your invalid password'     # type invalid password
     }
-    TRAINER_ID = '2752'                      # type your trainer id
-    TRAINER_TOKEN = '1a5e429f4f0305d92f8bf1823398af51'              # type your trainer token
+    TRAINER_ID = 'ID'                      # type your trainer id
+    TRAINER_TOKEN = 'YOUR TOKEN'              # type your trainer token
